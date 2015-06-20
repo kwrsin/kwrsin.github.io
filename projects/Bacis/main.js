91ac53049ec5c11283910bf9e4c892cd37e9b244
kwrsin <kwrsin@gmail.com>
2015-06-20 17:01:07 +0900

    Basic03

--- projects/Bacis/main.js
+++ projects/Bacis/main.js
@@ -2,27 +2,35 @@ enchant();
 window.onload = function() {
     var game = new Game(320, 320);
     game.preload('chara1.png');
-    game.onload = function () {
-        group = new Group();
-
+    game.onload = function() {
         bear1 = new Sprite(32, 32);
         bear1.image = game.assets['chara1.png'];
-        group.addChild(bear1);
+        bear1.addEventListener('enterframe', function() {
+            this.y++;
+        });
+        game.rootScene.addChild(bear1);
 
         bear2 = new Sprite(32, 32);
         bear2.image = game.assets['chara1.png'];
         bear2.x = 50;
         bear2.y = 20;
         bear2.frame = 5;
-
-        group.addChild(bear2);
-
-        group.addEventListener('enterframe', function() {
+        bear2.addEventListener('enterframe', function() {
             this.x++;
-            this.y++;
         });
-        game.rootScene.addChild(group);
+        game.rootScene.addChild(bear2);
 
+        subScene = new Scene();
+        label = new Label("Click here");
+        label.x = 80;
+        label.y = 150;
+        subScene.addChild(label);
+
+        game.pushScene(subScene);
+
+        subScene.addEventListener('touchend', function() {
+            game.popScene();
+        });
     }
     game.start();
 }
